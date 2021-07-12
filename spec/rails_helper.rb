9bb29068b8fda48636af2f14fbec999385d96e4d
Andres <GustavoAndres>
2021-07-12 12:20:07 -0500

    config models for rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,3 @@
-# require database cleaner at the top level
-require 'database_cleaner'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -42,12 +39,7 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
 RSpec.configure do |config|
-
-  config.include RequestSpecHelper, type: :request
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -79,20 +71,20 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  # add `FactoryBot` methods
-  config.include FactoryBot::Syntax::Methods
+   # add `FactoryBot` methods
+   config.include FactoryBot::Syntax::Methods
 
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transaction
-  end
+   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+   config.before(:suite) do
+     DatabaseCleaner.clean_with(:truncation)
+     DatabaseCleaner.strategy = :transaction
+   end
 
-  # start the transaction strategy as examples are run
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
+   # start the transaction strategy as examples are run
+   config.around(:each) do |example|
+     DatabaseCleaner.cleaning do
+       example.run
+     end
+   end
 
 end
