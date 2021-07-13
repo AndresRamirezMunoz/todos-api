6dbd6f080a112cca61b236cfd28578c0565ec4cb
Andres <GustavoAndres>
2021-07-13 09:38:24 -0500

    Add and config devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    resources :todos do
-      resources :items
+
+  resources :todos do
+    resources :items
+  end
+
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
+
 end
